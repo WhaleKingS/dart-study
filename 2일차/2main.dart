9571ed83a14dfd6775a1467303a58ef8634a38f6
WhaleKings <rnjsxodbs9898@gmail.com>
2025-07-08 13:02:30 +0900

    퀴즈풀이

---
+++ 2일차/2main.dart
@@ -0,0 +1,24 @@
+void main(){
+  // || = or 
+  // 사용자 회원가입시 이름과 전화번호 필수로 입력시 &&사용
+  String a = 'banan';
+
+  switch(a){
+    case '1':
+      print("object");
+      break;
+    case 'banana':
+      print("banana");
+      break;
+    default:
+      print("no case");
+  }
+
+  
+  if(a=='banana'){
+    print('banana');
+  } else if(a!='banana') {
+    print('none');
+  }
+
+}
