def0a6cfba9502775a73432ca87f4e1b0bb2af04
WhaleKings <rnjsxodbs9898@gmail.com>
2025-07-08 13:41:41 +0900

    코드수정

--- 2일차/2main1.dart
+++ 2일차/2main1.dart
@@ -2,9 +2,9 @@ void main(){
   var dan = 1;
   var count = 1;
   var result =  0;
+  //123123123123123123
   for(count = 1; count < 10; count +=1){
     result = count * dan;
     print("$dan X $count = ($result)");
   }
-    
   }
