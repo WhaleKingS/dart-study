bebc787dda93c36707872ca46b63b5241a829763
WhaleKings <rnjsxodbs9898@gmail.com>
2025-07-15 14:21:01 +0900

    http통신기본

---
+++ 7일차/book-quiz1.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+Future main() async {
+  var ip = InternetAddress.loopbackIPv4;
+  var port = 3000;
+
+  var server = await HttpServer.bind(ip, port);
+
+  print('\$ server activated - ${server.address.address}:${server.port}.');
+
+  await for (HttpRequest request in server) {
+    try {
+      if (request.uri.path.contains('/dan')) {
+        //request 에 따른 기능 구현
+        var varList = request.uri.path.split(',');
+        var dan = int.parse(varList[1]);
+        var result;
+        //1~9 까지 전부 곱하기 기능을 write 해야한다.
+        // for(var i = 1; i <= 9 ; i ++){
+        //   result
+        // }
+        //응답 작성
+        request.response.statusCode = HttpStatus.ok;
+        request.response.write("""
+$dan X 1 = ${dan * 1}
+$dan X 2 = ${dan * 2}
+$dan X 3 = ${dan * 3}
+$dan X 4 = ${dan * 4}
+$dan X 5 = ${dan * 5}
+$dan X 6 = ${dan * 6}
+$dan X 7 = ${dan * 7}
+$dan X 8 = ${dan * 8}
+$dan X 9 = ${dan * 9}
+""");
+      }
+
+      //우리는 사용자가 요청한 내용에 대한 응답을 다 했으니, 이 연결을 종료할게.
+      await request.response.close();
+    } catch (error) {
+      print(error);
+      print("에러 발생!");
+    }
+  }
+}
--- 7일차/main2.dart
+++ 7일차/main2.dart
@@ -1,1 +1,53 @@
+import 'dart:io';
 
+Future main() async {
+  var ip = InternetAddress.loopbackIPv4;
+  var port = 4040;
+
+  var server = await HttpServer.bind(ip, port);
+  await for (HttpRequest request in server) {
+    //try-catch 구조: try{} catch(error){}
+    //try 뒤에 있는 {}내용을 실행하되, 해당 내용 실행 중 에러가 발생하면
+    //catch 이후에 있는  {} 의 내용으로 바꿔서 실행해라.
+
+    try {
+      //http://127.0.0.1:4040/ 에서 ip와 port 를 제외한 이후 문자열이 / 만 있는지 확인하는 조건문
+      // request.uri.path -> 위의 url 에서 http://127.0.0.1:4040 을 제외한 그 다음 문자열 즉/
+      if (request.uri.path == '/') {
+        print('\$http response is Hello, World!".');
+        print('\$ send "200 OK".');
+
+        request.response
+          ..statusCode = HttpStatus.ok
+          ..write("Hello, World!");
+      } else if (request.uri.path.contains('/add')) {
+        print("\$ http response is result of 'add' operation.");
+        print("\$ send '200 ok'.");
+
+        var varList = request.uri.path.split(',');
+        var result = int.parse(varList[1]) + int.parse(varList[2]);
+
+        request.response
+          ..statusCode = HttpStatus.ok
+          ..write("${varList[1]} + ${varList[2]} = $result");
+      } else if (await File(request.uri.path.substring(1)).exists() == true) {
+        // request.uri.path -> port 번호까지 다 지운 후 남아있는 텍스트 즉 /sample.txt
+        // .substring(1) -> 맨 앞에 잇는 첫번째 문자열을 제거하겠다. 즉 sample.txt
+
+        print("\$ http response is '${request.uri.path}' file transfer.");
+        print("\$ send '200 ok'.");
+
+        var file = File(request.uri.path.substring(1));
+        var fileContent = await file.readAsString();
+
+        request.response
+          ..statusCode = HttpStatus.ok
+          ..headers.contentType = ContentType('text', 'plain', charset: "utf-8")
+          ..write(fileContent);
+      }
+      await request.response.close();
+    } catch (error) {}
+  }
+
+  print('\$ server activated - ${server.address.address}:${server.port}.');
+}
